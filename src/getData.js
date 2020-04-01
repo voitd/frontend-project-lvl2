8e2f607b6299daad6c8c9693e63605e4ef466f37
DVo1 <dimytr.voitenko@gmail.com>
2020-04-02 02:46:01 +0300

    rewrite logic

---
+++ src/getData.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
+import ini from 'ini';
+
+const getData = (filename) => {
+  const configPath = path.resolve(process.cwd(), filename);
+  const data = fs.readFileSync(configPath, 'utf-8');
+  const ext = path.extname(configPath);
+  const file = {
+    '.json': (fileName) => JSON.parse(fileName),
+    '.yml': (fileName) => yaml.safeLoad(fileName),
+    '.ini': (fileName) => ini.parse(fileName),
+  };
+  const readData = file[ext];
+  return readData(data);
+};
+export default getData;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
 import _ from 'lodash';
-import parsers from './parsers.js';
+import getData from './getData.js';
 
-import _ from 'lodash';
-import fs from 'fs';
-import path from 'path';
-import yaml from 'js-yaml'
-import ini from 'ini'
-
-const getData = (filename) => {
-  const configPath = path.resolve(process.cwd(), filename);
-  const data = fs.readFileSync(configPath, 'utf-8');
-  const ext = path.extname(configPath);
-  const file = {
-    '.json': (file) => JSON.parse(file),
-    '.yml': (file) => yaml.safeLoad(file),
-    '.ini': (file) => ini.parse(file),
-  }
-  const readData = file[ext];
-  return readData(data)
-};
 const props = [
   {
     type: 'unchenged',
     check: (before, after, key) => (_.has(before, key) && _.has(after, key) && (before[key] === after[key])),
-    process: (before, after) => before,
+    process: (before) => before,
     prefix: '   ',
   },
   {
@@ -41,43 +23,44 @@ const props = [
   {
     type: 'deleted',
     check: (before, after, key) => (_.has(before, key) && !_.has(after, key)),
-    process: (before, after) => before,
+    process: (before) => before,
     prefix: '  -',
   },
 ];
 
 const parse = (before, after) => {
-  const tempObj = { ...before, ...after };
-  const configKeys = _.keys(tempObj);
+  const configKeys = _.keys({ ...before, ...after });
   const getProps = (key) => props.find(({ check }) => check(before, after, key));
   return configKeys.map((key) => {
-    const { prefix, type, process } = getProps(key)
-    const value = process(before[key], after[key], parse)
-    return { key: key, type, value, prefix }
-  })
+    const { prefix, type, process } = getProps(key);
+    const value = process(before[key], after[key], parse);
+    return {
+      key, type, value, prefix,
+    };
+  });
 };
 const render = (tree) => {
-  const space = '  '
-
   const renderString = tree.map((node) => {
-    const { key, type, value, prefix } = node
+    const {
+      key, type, value, prefix,
+    } = node;
     const renderType = {
-      unchenged: `${prefix}${key}: ${value}`,
-      chenged: `${prefix}${key}: ${value.afterValue},\n  -${key}: ${value.beforeValue}`,
-      added: `${prefix}${key}: ${value}`,
-      deleted: `${prefix}${key}: ${value}`,
-    }
-    return renderType[node.type]
-  })
+      unchenged: `${prefix} ${key}: ${value}`,
+      chenged: `${prefix} ${key}: ${value.beforeValue}\n  - ${key}: ${value.afterValue}`,
+      added: `${prefix} ${key}: ${value}`,
+      deleted: `${prefix} ${key}: ${value}`,
+    };
+    return renderType[node.type];
+  });
 
-  const renderBody = `{\n${renderString.join('\n')}\n}`
-  return renderBody
-};                
+  const renderBody = `{\n${renderString.join('\n')}\n}`;
+  return renderBody;
+};
 
 const genDiff = (firstConfig = {}, secondConfig = {}) => {
   const before = getData(firstConfig);
   const after = getData(secondConfig);
   const ast = parse(before, after);
   return render(ast);
-}
+};
 export default genDiff;
